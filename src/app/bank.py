00573b20f78554e78c14bbc9a441098a51d3031b
rafa <raroito@gmail.com>
2019-02-06 13:50:19 +0100

    changed: added some methods
    added methos to print out the information regarding account and transactions

--- src/app/bank.py
+++ src/app/bank.py
@@ -17,3 +17,19 @@ class Bank:
         transaction = (sender, recipient, subject, amount)
         self.transactions.append(transaction)
         return transaction
+
+    def _print_info_account(self, account):
+        print('account number', account['number'], ' / ', account['firstname'], ' ', account['lastname'])
+
+    def print_info_accounts(self):
+        print('number od accounts', len(self.accounts))
+        for account in self.accounts:
+            self._print_info_account(account)
+
+    def _print_info_transaction(self, transaction):
+        print('sender #: ', transaction[0]['number'], ' to recipient #: ' , transaction[1]['number'], ', subject: ', transaction[2], ', amount: ', transaction[3])
+
+    def print_info_transactions(self):
+        print('number of transactions performed: ', len(self.transactions))
+        for transaction in self.transactions:
+            self._print_info_transaction(transaction)
